be1226a6179604256874a2f011442e942d097a84
decafandmac <eipeti.s2@gmail.com>
2022-07-14 02:35:30 +0900

    add service-user

--- src/main/kotlin/com/decafandmac/springcloudgateway/SpringCloudGatewayApplication.kt
+++ src/main/kotlin/com/decafandmac/springcloudgateway/SpringCloudGatewayApplication.kt
@@ -2,10 +2,11 @@ package com.decafandmac.springcloudgateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 class SpringCloudGatewayApplication
 
 fun main(args: Array<String>) {
